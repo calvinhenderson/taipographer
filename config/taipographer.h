9e4feb9296dd2e808c54c5de81188e8f2c67a68d
Calvin Henderson <henderson.calvin.w@gmail.com>
2025-07-25 23:11:20 -0400

    initial commit

---
+++ config/taipographer.h
@@ -0,0 +1,53 @@
+#pragma once
+
+// LAYERS
+#define LINUX 0
+#define NAV 1
+#define FN 2
+#define BASE_LAYERS LINUX
+
+// TIMEOUTS
+#define TIMEOUT_SHORT 20
+#define TIMEOUT_NORMAL 50
+#define TIMEOUT_LONG 120
+#define TIMEOUT_XLONG 1500
+
+// Labels for switch directions according to the matrix
+
+// Side, finger, outer/inner, top/bottom
+
+#define L_P_OT 0
+#define L_P_IT 1
+#define L_R__T 2
+#define L_M__T 3
+#define L_I_IT 4
+#define L_I_OT 5
+
+#define L_P_OB 6
+#define L_P_IB 7
+#define L_R__B 8
+#define L_M__B 9
+#define L_I_IB 10
+#define L_I_OB 11
+
+#define L_T_I_ 12
+#define L_T_C_ 13
+#define L_T_O_ 14
+
+#define R_I_OT 15
+#define R_I_IT 16
+#define R_M__T 17
+#define R_R__T 18
+#define R_P_IT 19
+#define R_P_OT 20
+
+#define R_I_OB 21
+#define R_I_IB 22
+#define R_M__B 23
+#define R_R__B 24
+#define R_P_IB 25
+#define R_P_OB 26
+
+#define R_T_O_ 27
+#define R_T_C_ 28
+#define R_T_I_ 29
